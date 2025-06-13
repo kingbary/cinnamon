11c7937dab659fab2e73809883b9bea3a65798b5
kingbary <kingsleyakwa@gmail.com>
2025-06-13 15:03:11 +0100

    feat: create nav link

---
+++ components/universal/nav-link.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import LogoText from './logo-text'
+import { navLinks } from '@/app/constants/navLink'
+import Link from 'next/link'
+import { Dot } from '../vectors/dot'
+import { usePathname } from 'next/navigation'
+
+export default function NavLink() {
+    const pathname = usePathname();
+    const active = navLinks.find((item) => item.route === pathname)?.route
+    return (
+        <nav className='relative w-full flex justify-center items-center'>
+            <span className='absolute -top-2 left-0'>
+                <LogoText />
+            </span>
+            <ul className='flex gap-2 items-center'>
+                {navLinks.map((navLink, index) => (
+                    <li className={`flex items-center gap-1 px-3 py-2 text-[#B8B8CC] ${active === navLink.route && 'text-white font-semibold'}`} key={index}>
+                        {active === navLink.route && (<Dot />)}
+                        <Link href={navLink.route}>{navLink.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
