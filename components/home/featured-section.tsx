f6da548b34cda331d19c0aa3eef50ca34355a550
kingbary <kingsleyakwa@gmail.com>
2025-06-15 16:07:19 +0100

    chore: update featured section

--- components/home/featured-section.tsx
+++ components/home/featured-section.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from '../universal/container'
 import { Button } from '../ui/button'
 import VideoCard from '../universal/video-card'
+import Image from 'next/image'
 
 export default function FeaturedSection() {
     const videoCardData = {
@@ -16,7 +17,9 @@ export default function FeaturedSection() {
         }
     }
     return (
-        <div className='bg-[#FFD480] rounded-t-[80px] rounded-b-[40px] mb-8'>
+        <div className='relative bg-[#FFD480] rounded-t-[80px] rounded-b-[40px] mb-8 overflow-hidden'>
+            <Image className='absolute top-0 left-0' src={'/images/headphone.png'} width={600} height={600} alt='headphone' />
+            <Image className='absolute bottom-0 right-0' src={'/images/retro-mic.png'} width={600} height={600} alt='headphone' />
             <Container className='flex flex-col w-full gap-8 items-center px-8 pt-20 pb-8'>
                 <div className='flex flex-col w-full items-center gap-6'>
                     <h2 className='text-center text-[#402F0D] max-w-[640px]'>Lorem ipsum consectetur elit eleifend donec dui vulputate.</h2>
@@ -25,10 +28,10 @@ export default function FeaturedSection() {
                         <Button variant={'secondary'}>Feature on our Podcast</Button>
                         <Button variant={'transparentBg'} className='bg-[#0000001A]'>View all Episodes</Button>
                     </div>
-                <div className='flex w-full justify-between gap-8'>
-                    <VideoCard props={videoCardData} />
-                    <VideoCard props={videoCardData} />
-                </div>
+                    <div className='flex w-full justify-between gap-8'>
+                        <VideoCard props={videoCardData} />
+                        <VideoCard props={videoCardData} />
+                    </div>
                 </div>
             </Container>
         </div>
--- components/vectors/line-stroke.tsx
+++ components/vectors/line-stroke.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from "react";
 export function LineStroke(props: SVGProps<SVGSVGElement>) {
     return (
         <svg {...props} width="2" height="18" viewBox="0 0 2 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 1L0.999999 17" stroke="#DADAE6" stroke-linecap="round" />
+            <path d="M1 1L0.999999 17" stroke="#DADAE6" strokeLinecap="round" />
         </svg>
     );
 }
