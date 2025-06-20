443ab4071175d486a2e26682ea1e703ed6125803
kingbary <kingsleyakwa@gmail.com>
2025-06-20 09:31:01 +0100

    chore: update logo text for blog page

--- components/universal/logo-text.tsx
+++ components/universal/logo-text.tsx
@@ -4,11 +4,12 @@ import React from 'react'
 
 export default function LogoText({ className }: { className?: string }) {
     const pathname = usePathname();
+    console.log("pathName:", pathname)
 
     let textColor = "text-white";
     if (pathname.includes("podcast")) {
         textColor = "text-black";
-    } else if (pathname.includes("blog")) {
+    } else if (pathname.includes("/blog/")) {
         textColor = "text-primary";
     }
     return (
