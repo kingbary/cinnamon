799fa01d866defe701bd5b6b68f9527b7f5b5a4a
kingbary <kingsleyakwa@gmail.com>
2025-09-03 15:15:23 +0100

    chore: remove blog header text

--- components/blog/blog-header.tsx
+++ components/blog/blog-header.tsx
@@ -7,7 +7,7 @@ export default function BlogHeader() {
             <div className=' blogHeader relative bg-[linear-gradient(180deg, rgba(65, 169, 213, 0.70) 0 %, rgba(65, 169, 213, 0.00) 75 %)] rounded-3xl mt-2 mb-8 overflow-hidden'>
                 <Container className='flex flex-col w-full gap-8 items-center px-8 pt-20 pb-[112px] mt-4'>
                     <div className='flex flex-col w-full items-center gap-6'>
-                        <h2 className='text-center text-[#402F0D] text-[120px] font-bold leading-[120px] -tracking-[10px] uppercase font-poppins'>Blog Header</h2>
+                        {/* <h2 className='text-center text-[#402F0D] text-[120px] font-bold leading-[120px] -tracking-[10px] uppercase font-poppins'>Blog Header</h2> */}
                     </div>
                 </Container>
             </div>
