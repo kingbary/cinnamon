3ec5fca85b387b36bf261f9e781d5120e821c087
kingbary <kingsleyakwa@gmail.com>
2025-09-26 09:51:02 +0100

    chore: update social links

--- components/universal/footer.tsx
+++ components/universal/footer.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 export default function Footer() {
     const socials = [
-        { name: "Instagram", link: "https://instagram.com/cinnamon" },
+        { name: "Instagram", link: "https://www.instagram.com/cinnamon_io" },
         { name: "YouTube", link: "https://youtube.com/cinnamon" },
-        { name: "TikTok", link: "https://tiktok.com/cinnamon" },
-        { name: "Facebook", link: "https://facebook.com/cinnamon" },
-        { name: "X (fka. Twitter)", link: "https://x.com/cinnamon" },
+        { name: "LinkedIn", link: "https://www.linkedin.com/company/cinnamonpr/" },
+        // { name: "Facebook", link: "https://facebook.com/cinnamon" },
+        // { name: "X (fka. Twitter)", link: "https://x.com/cinnamon" },
     ]
 
     // const otherLinks = [
